8acbfc6b13e0ce9611c101fcfa4ae34947006179
Luke Towers <luke@luketowers.ca>
2017-03-07 11:00:18 -0600

    Add language files

---
+++ lang/en/lang.php
@@ -0,0 +1,22 @@
+<?php 
+return [
+	'plugin'      => [
+		'name'        => 'Google Domain Verification',
+		'description' => 'Provide responses to Google Domain Verification requests',
+	],
+	
+	'permissions' => [
+		'tab'          => 'Google Domain Verification',
+		'manage_codes' => 'Manage Verification Codes',
+	],
+	
+	'settings'    => [
+		'description'        => 'Manage Google Domain Verification codes',
+		'verification_codes' => 'Verification Codes',
+		'code'               => 'Code',
+		'code_prompt'        => 'Add new code',
+		'code_comment'       => 'Paste the Domain Verification Code (the filename of the HTML file to be uploaded without the .html at the end)',
+		'code_description'   => 'Code Description (optional)',
+		'code_placeholder'   => 'google1d4b46d16655aed1',
+	],
+];
