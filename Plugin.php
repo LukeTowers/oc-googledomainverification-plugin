ab7f99d1dcb7dad402cf7c314a50c6b890c1f9a1
Luke Towers <luke@luketowers.ca>
2018-04-10 19:08:54 -0600

    Add plugin contents

---
+++ Plugin.php
@@ -0,0 +1,59 @@
+<?php namespace LukeTowers\GoogleDomainVerification;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * GoogleDomainVerification Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'luketowers.googledomainverification::lang.plugin.name',
+            'description' => 'luketowers.googledomainverification::lang.plugin.description',
+            'author'      => 'LukeTowers',
+            'icon'        => 'icon-check',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return [
+            'luketowers.googledomainverification.manage_codes' => [
+                'tab'   => 'luketowers.googledomainverification::lang.permissions.tab',
+                'label' => 'luketowers.googledomainverification::lang.permissions.manage_codes'
+            ],
+        ];
+    }
+    
+    /**
+	 * Registers the settings used by this plugin
+	 *
+	 * @return array
+	 */
+    public function registerSettings()
+    {
+        return [
+            'settings' => [
+                'label'       => 'luketowers.googledomainverification::lang.plugin.name',
+                'description' => 'luketowers.googledomainverification::lang.settings.description',
+                'icon'        => 'icon-check',
+                'class'       => 'LukeTowers\GoogleDomainVerification\Models\Settings',
+                'keywords'    => 'google domain verification code',
+                'permissions' => ['luketowers.googledomainverification.manage_codes'],
+            ],
+        ];
+    }
+}
---
+++ models/Settings.php
@@ -0,0 +1,21 @@
+<?php namespace LukeTowers\GoogleDomainVerification\Models;
+	
+use Model;
+
+/**
+ * Class Settings
+ */
+class Settings extends Model
+{
+	public $implement = ['System.Behaviors.SettingsModel'];
+	
+	/**
+	 * @var string $settingsCode Unique code to namespace settings under
+	 */
+	public $settingsCode = 'luketowers_googledomainverifications';
+	
+	/**
+	 * @var string $settingsFields Reference to field configuration
+	 */
+	public $settingsFields = 'fields.yaml';
+}
